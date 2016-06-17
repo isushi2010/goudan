e5685ef74e798e1fb828f77cdfa2cb57aa3df1c1
Xueting Wang <wangxueting@Nans-MacBook-Pro.local>
2016-06-18 01:26:10 +0800

    凶巴巴

---
+++ day1/import_tornado.py
@@ -0,0 +1,17 @@
+import tornado.ioloop
+import tornado.web
+
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("goudan.html")
+
+def make_app():
+    return tornado.web.Application([
+        (r"/", MainHandler),
+    ])
+
+if __name__ == "__main__":
+    app = make_app()
+    app.listen(8866)
+    tornado.ioloop.IOLoop.current().start()
